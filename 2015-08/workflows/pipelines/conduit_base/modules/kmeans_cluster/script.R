f0cfb2a1f08d12722c7c3b4b2bac555ae10cabf5
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-08-03 13:38:10 +1200

    dirname change

--- 2015-08/workflows/pipelines/conduit_base/modules/kmeans_cluster/script.R
+++
@@ -1,3 +0,0 @@
-data_frame <- readRDS("/home/ahin017/uoastorage/openapi/documentation/litreview/workflows/pipelines/conduit_base/modules/readCSV/data_frame.rds")
-cluster <- kmeans(data_frame, 3)
-saveRDS(cluster, file = "cluster.rds")
